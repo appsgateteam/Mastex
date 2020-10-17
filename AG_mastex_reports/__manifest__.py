356986411c8d50c9ff9912316b8568efdd0bd433
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2020-10-17 10:58:57 +0400

    invoice arabic report

---
+++ AG_mastex_reports/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'AG_mastex_reports',
+    'author': 'Fouad',
+    'depends': ['sale'],
+    'data': [
+        
+        'report/invoice_inhert.xml',
+        # 'report/cridet_note_inheirt.xml',
+        # 'report/purchase_quotation.xml',
+       
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
