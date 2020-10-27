66accc549bb7d69a4238717fc00a6047d94f4207
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-10-27 08:14:12 +0000

    Update __manifest__.py
    manifest update

--- muk_autovacuum/__manifest__.py
+++ muk_autovacuum/__manifest__.py
@@ -19,7 +19,7 @@
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ###################################################################################
-
+#used as dependency
 { 
     'name': 'MuK Autovacuum',
     'summary': 'Configure automatic garbage collection',
