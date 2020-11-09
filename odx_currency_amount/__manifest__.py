7d6c82b8da70157ff2f171aa69fa292033f7de00
bincy296 <71172217+bincy296@users.noreply.github.com>
2020-11-09 18:28:05 +0000

    Add files via upload
    updated by niyas

---
+++ odx_currency_amount/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Currency Amount Customisation',
+    'version': '13.0',
+    'category': 'Account',
+    'depends': ['base','account'],
+    'author': 'Odox SoftHub',
+    'website': 'http://www.odoxsofthub.com',
+    'license': 'GPL-3',
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+}
