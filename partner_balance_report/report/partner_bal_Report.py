7a9d2e0de69a4d2c0c6f81d169dd261d0738b5bc
Ziad-Monim <56960524+Ziad-Monim@users.noreply.github.com>
2021-01-25 09:11:54 +0400

    edit partner report

--- partner_balance_report/report/partner_bal_Report.py
+++ partner_balance_report/report/partner_bal_Report.py
@@ -33,7 +33,6 @@ class PartnerBalReport(models.AbstractModel):
         # raise UserError(to)
         self.env.cr.execute("""select 
                             init.partner_id as partner_id,
-                            trn.partner_id as partner_ids,
                             sum(init.debit) as init_dr,
                             sum(init.credit) as init_cr,
                             sum(init.balance) as init_bal,
@@ -51,7 +50,7 @@ class PartnerBalReport(models.AbstractModel):
                             and trn.account_internal_type in ('receivable','payable')
                             and trn.parent_state='posted' 
                             and trn.date between to_date('%s','yyyy-mm-dd') and to_date('%s','yyyy-mm-dd')
-                        group by init.partner_id,trn.partner_id"""%(start_date,start_date,end_date))
+                        group by init.partner_id"""%(start_date,start_date,end_date))
         # querys = self.env.cr.execute(query,)
         result = self.env.cr.dictfetchall()
 
@@ -65,10 +64,7 @@ class PartnerBalReport(models.AbstractModel):
             for rec in data['form']['partner']:
                 for res in result:
                     # raise UserError("%s %s"%(res['partner_id'],rec['id']))
-                    p_id = rec['id']
-                    pa_id = res['partner_id']
-                    par_id = res['partner_ids']
-                    if p_id == pa_id or p_id == par_id :
+                    if int(rec['id']) == int(res['partner_id']):
                         vals = {
                             'init_dr':res['init_dr'],
                             'init_cr':res['init_cr'],
